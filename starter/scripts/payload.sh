ffa4fbb6aeb1fe251783a7d620726a55d7a2d22d
Michael Obst <michael.obst@telekom.de>
2025-05-14 21:53:25 +0200

    fix incident respone script

--- starter/scripts/payload.sh
+++ starter/scripts/payload.sh
@@ -1,9 +1,24 @@
-#!/bin/bash
-#start monero_cpu_moneropool
-kubectl run --kubeconfig kube_config_cluster.yml moneropool --image=servethehome/monero_cpu_moneropool:latest --replicas=1
-#start minergate
-kubectl run --kubeconfig kube_config_cluster.yml minergate --image=servethehome/monero_cpu_minergate:latest --replicas=1
-#start cryptotonight
-kubectl run --kubeconfig kube_config_cluster.yml cryptotonight --image=servethehome/universal_cryptonight:latest --replicas=1
+# #!/bin/bash
+# #start monero_cpu_moneropool
+# kubectl run --kubeconfig kube_config_cluster.yml moneropool --image=servethehome/monero_cpu_moneropool:latest --replicas=1
+# #start minergate
+# kubectl run --kubeconfig kube_config_cluster.yml minergate --image=servethehome/monero_cpu_minergate:latest --replicas=1
+# #start cryptotonight
+# kubectl run --kubeconfig kube_config_cluster.yml cryptotonight --image=servethehome/universal_cryptonight:latest --replicas=1
 
-echo "Can you identify the payload(s)?"
+# echo "Can you identify the payload(s)?"
+
+#!/bin/bash
+# Start monero_cpu_moneropool
+kubectl create deployment moneropool --kubeconfig kube_config_cluster.yml --image=servethehome/monero_cpu_moneropool:latest
+kubectl scale deployment moneropool --kubeconfig kube_config_cluster.yml --replicas=1
+
+# Start minergate
+kubectl create deployment minergate --kubeconfig kube_config_cluster.yml --image=servethehome/monero_cpu_minergate:latest
+kubectl scale deployment minergate --kubeconfig kube_config_cluster.yml --replicas=1
+
+# Start cryptotonight
+kubectl create deployment cryptotonight --kubeconfig kube_config_cluster.yml --image=servethehome/universal_cryptonight:latest
+kubectl scale deployment cryptotonight --kubeconfig kube_config_cluster.yml --replicas=1
+
+echo "Deployments created and scaled."
